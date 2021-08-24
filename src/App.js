cdb1f726173458f9001d57a7a8f41e8fc42cf381
Zohidjon Toshpulatov <zohid99jon@gmail.com>
2021-08-24 17:55:22 +0500

    Broken our homepgae into smaller components, finished styling our menu-item components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <HomePage/>
     </div>
   );
