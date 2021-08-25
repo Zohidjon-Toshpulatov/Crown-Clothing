b3533f6c3fff7485416e3de8b3425c4fa88ca2f8
Zohidjon Toshpulatov <zohid99jon@gmail.com>
2021-08-25 11:17:57 +0500

    Upadate our app with shop pafe, Collection-perview and collection-item components added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
-
-const HatsPage = () => (
-  <div>
-    <h1>
-      HatsPage
-    </h1>
-  </div>
-)
 
 
 function App() {
@@ -17,7 +10,7 @@ function App() {
     <div className='App'>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
